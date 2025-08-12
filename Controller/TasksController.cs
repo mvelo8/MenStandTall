27aaf1f9701d1e2a0e902133722b7733b193c23a
mvelo8 <st10397913@vcconnect.edu.za>
2025-08-12 23:21:54 +0200

    First push

---
+++ Controller/TasksController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Wilproject.Hubs;
+
+namespace Wilproject.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly IHubContext<TaskHubs> _hubContext;
+
+        public TasksController(IHubContext<TaskHubs> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Task task)
+        {
+            // Here you would typically save the task to a database
+
+            await _hubContext.Clients.All.SendAsync("TaskAdded", task);
+            return Ok(task);
+        }
+    }
+}
---
+++ Hubs/TaskHubs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Wilproject.Hubs
+{
+    public class TaskHubs : Hub
+    {
+        public async Task AddTask(Task task)
+        {
+            await Clients.All.SendAsync("TaskAdded", task);
+        }
+    }
+}
